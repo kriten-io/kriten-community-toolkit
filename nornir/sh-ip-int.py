f10dfdeb8deb6a2f8ae01dea8ecfc9af1f8aeac0
Steve Corp <steve@kubecode.io>
2025-03-17 14:05:47 +0000

    Nornir example.

---
+++ nornir/sh-ip-int.py
@@ -0,0 +1,19 @@
+import json
+import os
+from nornir import InitNornir
+from nornir_utils.plugins.functions import print_result
+from nornir_netmiko import netmiko_send_command
+from nornir.core.filter import F
+
+extra_vars = os.environ.get('EXTRA_VARS')
+
+nr = InitNornir(config_file="./config.yml")
+
+if extra_vars:
+    extra_vars_data = json.loads(extra_vars)
+    group = extra_vars_data["group"]
+    arista_switch = nr.filter(F(groups__contains=group))
+
+result = arista_switch.run(netmiko_send_command, command_string="sh ip int brief")
+
+print_result(result)
