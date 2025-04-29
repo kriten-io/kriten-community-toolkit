980c6bd1f2db9617b82ab51c16c27fdf62ba8de6
Vladimir Kupriyanov <vlad@kubecode.io>
2025-04-29 16:31:35 +0100

    Create infrahub_nornir_example.py

---
+++ infrahub-nornir/infrahub_nornir_example.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+import json
+import os
+
+from nornir import InitNornir
+from nornir.core.inventory import Host
+from nornir_infrahub.plugins.tasks import get_artifact
+from nornir_utils.plugins.functions import print_result
+
+
+def filter_by_infrahub_node_id(host: Host, node_id: str) -> bool:
+    node = host.get("InfrahubNode")
+    if not node:
+        raise ValueError(f"Host {host.name} does not contain a valid InfrahubNode")
+
+
+    if not hasattr(node, "id"):
+        raise ValueError(f"InfrahubNode {node} does not have an id")
+
+    return node.id == node_id
+
+    
+def main():
+    extra_vars = os.getenv("EXTRA_VARS")
+    extra_vars = json.loads(extra_vars)
+    token = os.getenv("INFRAHUB_API_TOKEN")
+  
+    data = extra_vars.get("data", {})
+    node_id = data.get("target_id")
+    artifact_id = data.get("node_id")
+
+    if not node_id:
+        raise RuntimeError(f"There is no target_id provided in extra_vars")
+
+    if not artifact_id:
+        raise RuntimeError(f"There is no artifact_id provided in extra_vars")
+
+    nr = InitNornir(
+        inventory={
+            "plugin": "InfrahubInventory",
+            "options": {
+                "address": "http://192.168.10.57:8000",
+                "token": token,
+                "host_node": {"kind": "InfraDevice"},
+                "schema_mappings": [
+                    {"name": "hostname", "mapping": "primary_address.address"},
+                    {"name": "platform", "mapping": "platform.nornir_platform"},
+                ]
+            },
+        }
+    )
+
+    nr = nr.filter(filter_func=filter_by_infrahub_node_id, node_id=node_id)
+
+    result = nr.run(task=get_artifact, artifact_id=artifact_id)
+    print_result(result)
+        
+    
+if __name__ == "__main__":
+    main()
